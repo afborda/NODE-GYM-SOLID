d5649be09f944ed2eea593a475fc1cb6fc0f053e
C1329353 Abner Borda Fonseca <c1329353@interno.bb.com.br>
2024-12-26 01:06:58 -0300

    Lidando com erros

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
-import { prisma } from "./lib/prisma";
 import fastify from "fastify";
-import { z } from "zod";
-import { register } from "./http/controller/register";
+import { ZodError } from "zod";
 import { appRoutes } from "./http/routes";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    reply
+      .status(400)
+      .send({ message: "Validation error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "prod") {
+    console.error(error);
+  } else {
+    //TODO: Send error to monitoring service
+  }
+
+  return reply.status(500).send({ message: "Internal server error" });
+});
